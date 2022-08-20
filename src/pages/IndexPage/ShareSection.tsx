c17caeda06c2c05a797701ee546459eb9f9f00b4
joshua1988 <jangkeehyo@gmail.com>
2022-08-21 00:38:39 +0900

    카카오톡 공유하기 버튼 구현

--- src/pages/IndexPage/ShareSection.tsx
+++ src/pages/IndexPage/ShareSection.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { COLORS } from 'src/assets/theme';
 import Section from 'src/components/Section';
+import { shareKakaoMessage } from 'utils/kakao';
 
 const RouteSection = () => {
   return (
     <Section backgroundColor={COLORS.highlight2}>
-      옵바 여기에 공유관련을 넣어주세요!
+      <KakaoShare />
     </Section>
   );
+};
+
+const KakaoShare = () => {
+  useEffect(() => {
+    if (!Kakao.isInitialized()) {
+      Kakao.init('dce43601c312444ac6482e20a9a6b250');
+    }
+  });
+
+  const shareLink = () => {
+    shareKakaoMessage();
+  };
+
+  return <button onClick={shareLink}>공유하기</button>;
 };
 
 export default RouteSection;
