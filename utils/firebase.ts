474239b566566294695e34f9c33b9e7c7f4e7e74
joshua1988 <jangkeehyo@gmail.com>
2022-08-21 00:56:42 +0900

    파이어베이스 lib 설치 및 초기화

---
+++ utils/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQ3p5MRL-sbx2CJbv3Jfm_nkr0dtLV-dE',
+  authDomain: 'fir-51aad.firebaseapp.com',
+  databaseURL: 'https://fir-51aad.firebaseio.com',
+  projectId: 'fir-51aad',
+  storageBucket: 'fir-51aad.appspot.com',
+  messagingSenderId: '540093913106',
+  appId: '1:540093913106:web:d225b528e0f76e7aaaa0cb',
+};
+
+const app = initializeApp(firebaseConfig);
